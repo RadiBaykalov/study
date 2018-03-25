29ef2ca907408161fd44c83d4cc3b3ce0aadae1e
Radi Baykalov <baykalov.rady@gmail.com>
2018-03-25 18:24:41 +0300

    Signed-off-by: Radi Baykalov <baykalov.rady@gmail.com>

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,36 @@
+    <nav class="navbar navbar-expand-md navbar-dark bg-info">
+      <a class="navbar-brand" href="#">CATAN</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-catan" aria-controls="navbar-catan" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbar-catan">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Tournaments</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Blog</a>
+          </li>
+
+<!--           <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown04">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li> -->
+
+        </ul>
+
+<!--         <form class="form-inline my-2 my-md-0">
+          <input class="form-control" type="text" placeholder="Search">
+        </form> -->
+      </div>
+    </nav>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<p class="alert">QQ</p>
+<div class="container">QQ</div>
+</body>
+</html>
